5e9c7de8ddfa1217137ecbf640aa347129eec7af
Jason <jmlawler@gmail.com>
2022-06-07 00:08:15 -0500

    Exercise 18 : fixed typo

--- src/screens/Exercise18Screen.js
+++ src/screens/Exercise18Screen.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {Text, StyleSheet, View} from 'react-native';
 
 const Exercise18Screen = () => {
-    const userName = 'Jason Lawler;
+    const userName = 'Jason Lawler';
 
     return (
         <View>
