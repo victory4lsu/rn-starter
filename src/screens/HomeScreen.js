ac05f687fee953a3d3dbcb9e65f171e769821179
Jason <jmlawler@gmail.com>
2022-06-08 00:45:56 -0500

    ButtonTypes

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,9 +1,24 @@
 import React from 'react';
-import { Text, StyleSheet } from 'react-native';
+import { Text, StyleSheet, View, Button, TouchableOpacity } from 'react-native';
 
-const HomeScreen = () => {
-  return <Text style={styles.text}>Hello Crunchies!!</Text>;
+const HomeScreen = ({navigation}) => {
+ 
+  return (
+    <View>
+      <Text style={styles.text}>Hello Crunchies!!</Text>
+      <Button 
+        title="Go To Components Demo" 
+        onPress={() => {navigation.navigate('Components')}}
+      />
+      <Button 
+        onPress={() => {navigation.navigate('List')}}
+        title="Go To List Demo"
+      />
+    </View>
+    );
 };
+
+
 
 const styles = StyleSheet.create({
   text: {
